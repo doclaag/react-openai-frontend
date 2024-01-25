858bed5dc2a2ac37065ccdcf42a8052f07212184
Luis Alonzo <doclaag@gmail.com>
2024-01-24 21:02:33 -0600

    Add pros-cons-discusser-steam use case and update ProsConsStreamPage

--- src/core/use-cases/index.ts
+++ src/core/use-cases/index.ts
@@ -1,2 +1,3 @@
 export * from "./orthography.use-case";
+export * from "./pros-cons-discusser-steam.use-case";
 export * from "./pros-cons-discusser.use-case";
--- src/presentation/pages/pros-cons-stream/ProsConsStreamPage.tsx
+++ src/presentation/pages/pros-cons-stream/ProsConsStreamPage.tsx
@@ -1,7 +1,64 @@
+import { useState } from 'react';
+import { GptMessage, MyMessage, TypingLoader, TextMessageBox } from '../../components';
+import { prosConsDiscusserStreamUseCase } from '../../../core/use-cases';
 
+interface Message {
+  text: string;
+  isGpt: boolean;
+}
 
 export const ProsConsStreamPage = () => {
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const handlePost = async (text: string) => {
+
+    setIsLoading(true);
+    setMessages((prev) => [...prev, { text, isGpt: false }]);
+
+    //TODO: useCase
+    await prosConsDiscusserStreamUseCase(text);
+    setIsLoading(false);
+
+    //TODO: isGpt =true
+
+  };
+
   return (
-    <div>ProsConsStreamPage</div>
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+          {/* Welcome */}
+          <GptMessage text="¿Qué deseas comparar hoy?" />
+
+          {
+            messages.map((message, index) => (
+              message.isGpt
+                ? (<GptMessage key={index} text={message.text} />)
+                : (<MyMessage key={index} text={message.text} />)
+            ))
+          }
+
+          {
+            isLoading && (
+              <div className="col-start-1 col-end-12">
+                <TypingLoader />
+              </div>
+            )
+          }
+
+        </div>
+      </div>
+
+      <TextMessageBox
+        onSendMessage={handlePost}
+        placeholder="Escribe tu texto aquí"
+        disableCorrections
+      />
+
+    </div>
   );
 };
+
+
