362b95a530cb17481c1fa46e78c9361b988997b6
Luis Alonzo <doclaag@gmail.com>
2024-01-28 21:58:06 -0600

    Add text-to-audio use case and related components

--- src/core/use-cases/index.ts
+++ src/core/use-cases/index.ts
@@ -2,4 +2,5 @@ export * from "./orthography.use-case";
 export * from "./pros-cons-discusser-steam-generator.use-case";
 export * from "./pros-cons-discusser-steam.use-case";
 export * from "./pros-cons-discusser.use-case";
+export * from "./text-to-audio.use-case";
 export * from "./translate-text.use-case";
---
+++ src/presentation/components/chat-bubbles/GptMessageAudio.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable tailwindcss/migration-from-tailwind-2 */
+import Markdown from 'react-markdown';
+
+interface Props {
+    text: string;
+    audio: string;
+}
+
+export const GptMessageAudio = ({ text, audio }: Props) => {
+    return (
+        <div className="col-start-1 col-end-8 rounded-lg p-3">
+            <div className="flex flex-row items-start">
+                <div className="flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full bg-green-600">
+                    G
+                </div>
+                <div className="relative ml-3 rounded-xl bg-black bg-opacity-25 px-4 pt-3 text-sm shadow">
+                    <Markdown>{text}</Markdown>
+                    <audio
+                        controls
+                        src={audio}
+                        className="w-full custom-audio"
+                        autoPlay
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
